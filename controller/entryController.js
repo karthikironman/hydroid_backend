2ea1c1bd1741562e4ad4a58f7b978c5fea7516cd
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-12-16 23:08:06 +0530

    register and signup api's

--- controller/entryController.js
+++ controller/entryController.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
 const jwt = require("jsonwebtoken");
 const admin = require("../model/admin");
+var validator = require('validator');
+const { v4: uuidv4 } = require('uuid');
+
 var signOptions = {
     // issuer: '',
     // subject: '',
@@ -17,14 +20,15 @@ var refreshSignOptions = {
 };
 
 
-const loginWithEmailAndPassword = async (req, res, next) => {
+const loginWithEmailAndPassword = async (req, res) => {
     const { email_id, password } = req.body;
     let userData;
     // Validate emil & password
     if (!email_id || !password) {
-        return res.status(400).send({
+        return res.status(200).send({
             success: false,
-            message: "Please provide an email and password"
+            message: "Please provide an email and password",
+            code: 'MISSING_FIELDS'
         });
     }
     try {
@@ -33,13 +37,20 @@ const loginWithEmailAndPassword = async (req, res, next) => {
 
         if (!userData) {
             return res
-                .status(401)
-                .send({ success: false, message: "Invalid credentials" });
+                .status(200)
+                .send({
+                    success: false, message: "Invalid credentials",
+                    code: 'WRONG_CRED'
+                });
         }
     } catch (err) {
         return res
-            .status(401)
-            .send({ success: false, message: "Invalid credentials! Login failed" });
+            .status(200)
+            .send({
+                success: false,
+                message: "Invalid credentials! Login failed",
+                code: 'WRONG_CRED'
+            });
     }
 
     try {
@@ -48,13 +59,21 @@ const loginWithEmailAndPassword = async (req, res, next) => {
 
         if (!isMatch) {
             return res
-                .status(401)
-                .send({ success: false, message: "Invalid credentials" });
+                .status(200)
+                .send({
+                    success: false,
+                    message: "Invalid credentials",
+                    code: 'WRONG_CRED'
+                });
         }
     } catch (err) {
         return res
-            .status(401)
-            .send({ success: false, message: "Invalid credentials! Login failed" });
+            .status(200)
+            .send({
+                success: false,
+                message: "Invalid credentials! Login failed",
+                code: 'WRONG_CRED'
+            });
     }
 
     let privateKEY = fs.readFileSync("./public/private.key", "utf-8");
@@ -71,10 +90,10 @@ const loginWithEmailAndPassword = async (req, res, next) => {
     )
 
     res.status(200).json({
-        user_id, 
-        name, 
-        account_type, 
-        email_id:userData.email_id,  //email_id is already defined, so exclusively including
+        user_id,
+        name,
+        account_type,
+        email_id: userData.email_id,  //email_id is already defined, so exclusively including
         createdAt,
         success: true,
         token,
@@ -82,6 +101,55 @@ const loginWithEmailAndPassword = async (req, res, next) => {
     });
 };
 
+const register = async (req, res) => {
+    if ('email_id' in req.body && 'password' in req.body && 'account_type' in req.body && 'name' in req.body) {
+        const user_id = uuidv4();
+        const { name, email_id, password, account_type } = req.body;
+        const validEmail = await validator.isEmail(email_id);
+        if (validEmail) {
+            if (['engineer', 'admin', 'user'].includes(account_type)) {
+                const isExisting = await admin.find({ email_id })
+                if (isExisting.length !== 0) {
+                    res.status(200).json({
+                        success: false,
+                        message: 'email_id is already present in the database',
+                        code: 'ALREADY_REGISTERED'
+                    })
+                } else {
+                    admin.create({ name, email_id, user_id, password, account_type })
+                        .then((a) => {
+                            res.status(200).json({
+                                message: a,
+                                success: true
+                            })
+                        })
+                }
+            }
+            else {
+                res.status(200).json({
+                    success: false,
+                    message: 'invalid account_type, it can be only engineer, admin or user',
+                    code: 'WRONG_AC_TYPE'
+                })
+            }
+        } else {
+            res.status(200).json({
+                success: false,
+                message: 'invalid email id',
+                code: 'INVALID_EMAIL'
+            })
+        }
+
+    } else {
+        res.status(200).json({
+            success: false,
+            message: 'need email_id, password, account_type, name in the body',
+            code: 'MISSING_VALUES'
+        })
+    }
+
+}
 module.exports = {
-    loginWithEmailAndPassword
+    loginWithEmailAndPassword,
+    register
 }
--- routes/entry.js
+++ routes/entry.js
@@ -1,4 +1,5 @@
 const entryController = require('../controller/entryController')
 module.exports = (app) => {
     app.post('/api/v1/login',entryController.loginWithEmailAndPassword)
+    app.post('/api/v1/register',entryController.register)
 }
