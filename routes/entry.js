986b5149a3e245817e82c097c5e24685d8abd9d3
karthik.selvaraj <karthik.selvaraj@diatoz.com>
2020-12-16 23:50:05 +0530

    health api

--- routes/entry.js
+++ routes/entry.js
@@ -2,4 +2,5 @@ const entryController = require('../controller/entryController')
 module.exports = (app) => {
     app.post('/api/v1/login',entryController.loginWithEmailAndPassword)
     app.post('/api/v1/register',entryController.register)
+    app.get("/api/v1/helth",(req,res)=>{res.send('UP-A')})
 }
